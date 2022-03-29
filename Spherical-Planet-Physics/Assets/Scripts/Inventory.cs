0d29962f63d936608bd72a18d0ac6e99e0fc05ec
NoelleStansel <noellestansel@gmail.com>
2022-03-28 22:34:07 -0400

    Revert "Merge fix"
    This reverts commit db2ec4872452a92a74ef4441f3f80ae3372a22e3, reversing
    changes made to 24833ad41128e36ec3e805a5270765ffcca73581.

--- Spherical-Planet-Physics/Assets/Scripts/Inventory.cs
+++ Spherical-Planet-Physics/Assets/Scripts/Inventory.cs
@@ -1,21 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
-    public Image slot1;
-    public Image slot2;
-    public Image slot3;
-    public Image slot4;
-
     private GameObject playerModel;
     private GameObject playerJetpackModel;
     private PlayerController controller;
-
-    private List<Pickup> inventoryList = new List<Pickup>();
-    private int currPickupIndex;
 
     void Awake()
     {
@@ -25,58 +16,15 @@ public class Inventory : MonoBehaviour
         playerJetpackModel.SetActive(false);
     }
 
-    //When the player collides with an object that has the "Pickup" tag, adds it to the inventory
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Pickup")
         {
-            AddPickup(other.gameObject.GetComponent<Pickup>());
-        }
-    }
-
-    //Adds an item with the "Pickup" script to the list of pickups up to a maximum of 4
-    /*When added to the inventory the item is deactivated, the models are swapped
-    the item is equipped and the inventory is displayed on the UI*/
-    private void AddPickup(Pickup pickup)
-    {
-        if (inventoryList.Count < 4)
-        {
-            inventoryList.Add(pickup);
-            pickup.gameObject.SetActive(false);
+            Destroy(other.gameObject);
             playerModel.SetActive(false);
             playerJetpackModel.SetActive(true);
             controller.EquipJetpack();
-            DisplayInventory();
-        }
-        else
-        {
-            Debug.Log("Inventory Full");
         }
     }
 
-    //Displays the pickup's sprite on the slot it is in
-    /*Does this by going through the inventory list and
-     setting the first UI slot equal to the sprite of the first item in the list.
-    it then repeats this until it has goe through the entire inventory list*/
-    private void DisplayInventory()
-    {
-        for (int i = 0; i < inventoryList.Count; i++)
-        {
-            switch (i)
-            {
-                case 0:
-                    slot1.sprite = inventoryList[i].pickupSprite;
-                    break;
-                case 1:
-                    slot2.sprite = inventoryList[i].pickupSprite;
-                    break;
-                case 2:
-                    slot3.sprite = inventoryList[i].pickupSprite;
-                    break;
-                case 3:
-                    slot4.sprite = inventoryList[i].pickupSprite;
-                    break;
-            }
-        }
-    }
 }
